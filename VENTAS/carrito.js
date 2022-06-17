c6502ca591561df4c3699a8abf65aa87d2795e89
73939061 <102633617+73939061@users.noreply.github.com>
2022-06-17 00:44:03 -0500

    hh

--- VENTAS/carrito.js
+++ VENTAS/carrito.js
@@ -1,7 +1,7 @@
-class carrito{
-    comprarProducto(e){
+class container{
+    añadir_carrito(e){
         e.preventDefault();
-        if(e.target.classList.containers("añadir-carrito")){
+        if(e.target.classList.container("añadir_carrito")){
             const producto = e.target.parentElement.parentElement;
             //this.leerDatosProducto(producto)
             console.log(producto);
